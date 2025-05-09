1616bdddfcb9bddbec0fe6b1f12ca593762928f9
Stefka Kaykieva <skaykieva@gmail.com>
2025-05-09 16:08:15 +0300

    fix: fix logo theming

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from 'react';
-import { ThemeId } from '@/styles/theme-config';
 import Image from 'next/image';
+import useTheme from '@/hooks/useTheme';
 
 export default function Logo() {
-  const [theme, setTheme] = useState<ThemeId>('light');
-
-  useEffect(() => {
-    const html = document.documentElement;
-    const currentTheme = html.classList.contains('theme-dark') ? 'dark' : 'light';
-    setTheme(currentTheme);
-
-    const observer = new MutationObserver(() => {
-      const updatedTheme = html.classList.contains('theme-dark') ? 'dark' : 'light';
-      setTheme(updatedTheme);
-    });
-    observer.observe(html, { attributes: true, attributeFilter: ['class'] });
-
-    return () => observer.disconnect();
-  }, []);
+  const { theme } = useTheme();
 
   return (
     <Image
